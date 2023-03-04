92f79252c728e79000df97c9782b832346ce63de
Devon-Scott <DevonJScott@hotmail.com>
2023-03-03 23:18:20 -0800

    troubleshooting recursive search

--- Cracking/Chapter2/returnKthToLast.cpp
+++ Cracking/Chapter2/returnKthToLast.cpp
@@ -10,13 +10,32 @@ printLinkedList(Node<T>* head)
 */
 #include "linkedUtility.h"
 
+template <typename T>
+T findKthToLast(const int k, Node<T>* head, int& index){
+    if (head->next != nullptr){
+        return findKthToLast(k, head->next, index);
+    }
+    index++;
+    std::cout << index << " " << k << std::endl;
+    if (index == k){
+        return head->value;
+    }
+}
+
+template <typename T>
+T findKthToLast(const int k, Node<T>* head){
+    if (k <= 0){
+        throw "can't return past the end";
+    }
+    Node<T>* iterator = head;
+    int index = 0;
+    return findKthToLast(k, iterator, index);
+}
+
 int main(){
 	std::vector<std::string> words = {"cat", "cat", "dog", "snake", "kitten", "cat", "bird", "mouse", "lizard", "fish", "rat", "cat", "dog", "cat", "kitten", "mouse", "kitten", "monkey", "monkey"};
 	std::vector<std::string> test = {"word", "word", "other"};
 	Node<std::string>* head = assignDataToList(words);
 	
-	printLinkedList(head);
-	// I'm cutting the list off at the point where I delete. I should be assigning 
-	// something like head = head->next->next
-	printLinkedList(head);
+    std::cout << findKthToLast(2, head) << std::endl;
 }
